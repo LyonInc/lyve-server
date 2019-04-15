cf36045835d8ef4d923d5c3bbf5e19805e2ead59
pastelcolors <localcomputer1@gmail.com>
2019-04-15 22:07:48 +0800

    Initial commit

---
+++ common/models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+const uuidV4 = require('uuid/v4');
+
+module.exports = function(User) {
+  User.observe('before save', function(ctx, cb) {
+    if (ctx.instance) {
+      ctx.instance.id = uuidV4();
+    } else {
+      ctx.data.id = uuidV4();
+    }
+    cb();
+  });
+};
